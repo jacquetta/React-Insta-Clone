3889b3a3eca72b436982ef02bb42bbb274da12a5
jacquetta <jacquettavb@hotmail.com>
2019-05-30 21:32:24 -0500

    refractoring code added comment section

---
+++ instagram2/src/Components/CommentSection/Comment.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './commentsection.css';
+
+const Comment = props => {
+    return (
+        <div>
+            <p><span className="userName">{props.comment.username}</span>  <span className="comment">{props.comment.text}</span></p>
+        </div>
+    );
+};
+
+Comment.propTypes = {
+    comment: PropTypes.shape({
+        text: PropTypes.string,
+        username: PropTypes.string
+    })
+};
+
+export default Comment;
